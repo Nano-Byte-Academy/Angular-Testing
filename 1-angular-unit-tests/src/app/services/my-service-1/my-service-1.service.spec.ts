818af270f392955d36b3efcefd42caef98185a0d
prosenjit005 <167856468+Nano-Byte-Academy@users.noreply.github.com>
2024-05-12 17:28:15 +0530

    Test Demo: HTTP GET

--- 1-angular-unit-tests/src/app/services/my-service-1/my-service-1.service.spec.ts
+++ 1-angular-unit-tests/src/app/services/my-service-1/my-service-1.service.spec.ts
@@ -1,53 +1,77 @@
 import { TestBed } from '@angular/core/testing';
 import { MyService1Service } from './my-service-1.service';
 import { MyLoggerServiceService } from './my-logger-service/my-logger-service.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { POSTS_LIST } from './PostsListData';
 
 describe('MyService1Service', () => {
   let myService1: MyService1Service,
-    myFakeLogger: any;
+    myFakeLogger: any, httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     myFakeLogger = jasmine.createSpyObj('MyLoggerServiceService', ['log']);
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
       providers: [
         MyService1Service,
         { provide: MyLoggerServiceService, useValue: myFakeLogger }
       ]
     });
     myService1 = TestBed.inject(MyService1Service);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(myService1).toBeTruthy();
   });
 
-  // Testing spyOn
-  it('#1 Testing spyOn example', () => {
-    const myLogger = new MyLoggerServiceService();
-    spyOn(myLogger, 'log');
+  it('Testing HTTP GET mock call', () => {
+    myService1.getAllPosts()
+      .subscribe(postList => {
+        expect(postList).toBeTruthy();
+        expect(postList[0].title).toBe('my title 1');
+      });
 
-    const myService1 = new MyService1Service(myLogger);
-    const myResult = myService1.getStringValue();
+    const req = httpTestingController.expectOne('https://jsonplaceholder.typicode.com/posts');
+    expect(req.request.method).toEqual('GET');
+    req.flush(POSTS_LIST);
+    //httpTestingController.verify();
 
-    expect(myResult).toBe('my string return value');
-    expect(myLogger.log).toHaveBeenCalledTimes(1);
   });
+
+  // Testing spyOn
+  // it('#1 Testing spyOn example', () => {
+  //   const myLogger = new MyLoggerServiceService();
+  //   spyOn(myLogger, 'log');
+
+  //   const myService1 = new MyService1Service(myLogger);
+  //   const myResult = myService1.getStringValue();
+
+  //   expect(myResult).toBe('my string return value');
+  //   expect(myLogger.log).toHaveBeenCalledTimes(1);
+  // });
 
   // Testing jasmine.createSpyObj()
-  it('#2 Testing jasmine.createSpyObj() example', () => {
-    const myFakeLogger = jasmine.createSpyObj('MyLoggerServiceService', ['log']);
+  // it('#2 Testing jasmine.createSpyObj() example', () => {
+  //   const myFakeLogger = jasmine.createSpyObj('MyLoggerServiceService', ['log']);
 
-    const myService1 = new MyService1Service(myFakeLogger);
-    const myResult = myService1.getStringValue();
+  //   const myService1 = new MyService1Service(myFakeLogger);
+  //   const myResult = myService1.getStringValue();
 
-    expect(myResult).toBe('my string return value');
-    expect(myFakeLogger.log).toHaveBeenCalledTimes(1);
-  });
+  //   expect(myResult).toBe('my string return value');
+  //   expect(myFakeLogger.log).toHaveBeenCalledTimes(1);
+  // });
 
   it('#getStringValue should return a string value', () => {
     expect(myService1.getStringValue())
       .withContext('Inside getStringValue() my test')
       .toBe('my string return value');
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
 });
--- 1-angular-unit-tests/src/app/services/my-service-1/my-service-1.service.ts
+++ 1-angular-unit-tests/src/app/services/my-service-1/my-service-1.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Observable, delay, of } from 'rxjs';
+import { Observable, delay, map, of } from 'rxjs';
 import { MyLoggerServiceService } from './my-logger-service/my-logger-service.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyService1Service {
 
-  constructor(private logger: MyLoggerServiceService) { }
+  constructor(private logger: MyLoggerServiceService,
+    private http: HttpClient
+  ) { }
+
+  getAllPosts(): Observable<any> {
+    return this.http.get('https://jsonplaceholder.typicode.com/posts');
+  }
 
   getStringValue() {
     this.logger.log("inside getStringValue() method");
