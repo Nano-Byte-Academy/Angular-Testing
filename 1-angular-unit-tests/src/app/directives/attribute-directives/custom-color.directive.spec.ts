2f83b7798bc8405b039299faa6117d3bde4b7522
prosenjit005 <167856468+Nano-Byte-Academy@users.noreply.github.com>
2024-05-19 19:00:26 +0530

    testing the attribute directive alone for all cases

--- 1-angular-unit-tests/src/app/directives/attribute-directives/custom-color.directive.spec.ts
+++ 1-angular-unit-tests/src/app/directives/attribute-directives/custom-color.directive.spec.ts
@@ -1,9 +1,54 @@
-// import { CustomColorDirective } from './custom-color.directive';
+import { Component } from '@angular/core';
+import { CustomColorDirective } from './custom-color.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
-// describe('CustomColorDirective', () => {
-//   it('should create an instance', () => {
-//     const mockElementRef = jasmine.createSpyObj('ElementRef', ['nativeElement']);
-//     const customDirective = new CustomColorDirective(mockElementRef);
-//     expect(customDirective).toBeTruthy();
-//   });
-// });
+@Component({
+    standalone: true,
+    template: `
+      <h2 appCustomColor>red color font 1</h2>
+      <h2 appCustomColor>red color font 2</h2>
+      <h2>Default color</h2>
+      `,
+    imports: [CustomColorDirective],
+})
+class TestComponent { }
+
+describe('CustomColorDirective', () => {
+    let fixture: ComponentFixture<TestComponent>;
+    let elemWithDirective: any;
+    let elemWithoutDirective: any;
+
+    beforeEach(() => {
+        fixture = TestBed.configureTestingModule({
+            imports: [CustomColorDirective, TestComponent],
+        }).createComponent(TestComponent);
+
+        fixture.detectChanges(); // initial binding
+
+        // all elements with CustomColorDirective directive
+        elemWithDirective = fixture.debugElement.queryAll(By.directive(CustomColorDirective));
+
+        // all h2 elements without CustomColorDirective directive
+        elemWithoutDirective = fixture.debugElement.queryAll(By.css('h2:not([appCustomColor])'));
+    });
+
+    it('should have 2 elements with the color directive', () => {
+        expect(elemWithDirective.length).toBe(2);
+    });
+
+    it('should have 1 element without the color directive', () => {
+        expect(elemWithoutDirective.length).toBe(1);
+    });
+
+    it('should color 1st <h2> "red"', () => {
+        const color = elemWithDirective[0].nativeElement.style.color;
+        expect(color).toBe('red');
+    });
+
+    it('should color last <h2> empty ""', () => {
+        const color = elemWithoutDirective[0].nativeElement.style.color;
+        expect(color).toBe('');
+    });
+
+});
